69a0c8def806ef7356f26b7e702ac8b2d9c7063c
LRyan <lwryan6@gmail.com>
2022-10-27 11:25:50 -0500

    Completed Part 1 - Connect Database to Spring App & Pass TestTaskOne tests

--- queries.sql
+++ queries.sql
@@ -1,7 +1,23 @@
 -- Part 1: Test it with SQL
 
+    -- Table: job
+    -- Columns:
+    -- id int PK
+    -- employer varchar(255)
+    -- name varchar(255)
+    -- skills varchar(255)
+
+    -- SELECT * FROM job;
+
+
 -- Part 2: Test it with SQL
+
+
 
 -- Part 3: Test it with SQL
 
+
+
 -- Part 4: Test it with SQL
+
+
